5bd8d846a2d25689985862221179b643ce366b66
Saga Liljenroth Dickman <58550856+SagaLD@users.noreply.github.com>
2023-12-14 16:25:29 +0100

    save stuff fixes and other

--- Assets/Code/Scripts/StartAndEnd.cs
+++ Assets/Code/Scripts/StartAndEnd.cs
@@ -328,7 +328,7 @@ public class StartAndEnd : MonoBehaviour
 
         Debug.Log("Start the thing!");
 
-        //FindAnyObjectByType<MainMenuData>().UpdateHighscore(SceneManager.GetActiveScene().name, score);
+        FindAnyObjectByType<MainMenuData>().UpdateHighscore(SceneManager.GetActiveScene().name, score);
 
         ShowScore();
 
